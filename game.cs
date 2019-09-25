c0d38e0a4f94a624773717acfea08a4e2221a183
Student <example@example.com>
2019-09-24 18:45:05 -0700

    practice

---
+++ game.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace higherLower
+{
+    class Game
+    {
+        public int CurrentAmount; 
+        public int HighestAmount;
+        public int LowestAmount;
+        public bool Repeat;
+    
+    public Game(int aCurrentAmount, int aHighestAmount, int aLowestAmount)
+    {
+        CurrentAmount = aCurrentAmount;
+        HighestAmount = aHighestAmount;
+        LowestAmount = aLowestAmount;
+        Repeat = true;
+      }
+      public static changeProperty(){
+          
+      }
+    }
+}
---
+++ higherLower.cs
@@ -0,0 +1,16 @@
+using System; 
+using System.Collections.Generic;
+
+namespace higherLower
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Game start = new Game(50,1,100);
+            while(start.Repeat = true){
+                
+            }
+        }
+    }
+}
